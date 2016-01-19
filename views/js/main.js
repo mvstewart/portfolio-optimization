8fd7d0217f16edb256d961baf506d3f15029bf82
Marina V Stewart <mvstewart@users.noreply.github.com>
2016-01-18 23:18:06 -0500

    Update main.js
    Move .mover style into the style.css

--- views/js/main.js
+++ views/js/main.js
@@ -534,9 +534,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var elem = document.createElement('img');
   elem.className = 'mover';
   elem.src = "images/pizza.png";
-  elem.style.height = "100px";
-  elem.style.width = "73.333px";
-  var movPizzas1 = document.getElementById("#movingPizzas1");
+  var movPizzas1 = document.getElementById("movingPizzas1");
   for (var i = 0; i < 200; i++) {
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
