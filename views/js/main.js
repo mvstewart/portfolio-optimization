1911ab605603b8c1a711e767fc94349eaa12ac5a
Marina V Stewart <mvstewart@users.noreply.github.com>
2016-01-21 17:14:50 -0500

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -446,10 +446,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randPizzaContain = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(randPizzaContain[0], size);
+    for (var i = 0; i < randPizzaContain.length; i++) {
+      var newwidth = (randPizzaContain[i].offsetWidth + dx) + 'px';
+      randPizzaContain[i].style.width = newwidth;
     }
   }
 
