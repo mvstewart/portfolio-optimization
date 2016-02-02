5803744eda016774553d4c86c4f6556397c01fa0
Marina V Stewart <mvstewart@users.noreply.github.com>
2016-02-01 21:22:32 -0500

    Update main.js
    Update the for-loop in the function that generates the sliding pizzas when the page loads.

--- views/js/main.js
+++ views/js/main.js
@@ -541,15 +541,17 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  // Move unnecessary for the loop items out of the for-loop
+  var elem = document.createElement('img');
+  elem.className = 'mover';
+  elem.src = "images/pizza.png";
+  elem.style.height = "100px";
+  elem.style.width = "73.333px";  
   for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    // Switch querySelector to getElementById
+    document.getElementById("movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
