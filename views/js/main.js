6de9a2dc786c74e0c4cf517bad7d1bb54c9aca6e
Marina V Stewart <mvstewart@users.noreply.github.com>
2016-02-01 22:29:25 -0500

    Update main.js
    Use transform to avoid jank in the updatePositions

--- views/js/main.js
+++ views/js/main.js
@@ -531,7 +531,8 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
     // Assign phase results by pulling from the array created in the new for-loop above
     var phase = phaseArray[i % 5];
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    // Use transform to avoid jank
+    items[i].style.tranform = 'translateX(' + 100 * phase + 'px)';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
