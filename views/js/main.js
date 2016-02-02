6004e11f488bf4ab35046f92cfc5a6d1bf8b595e
Marina V Stewart <mvstewart@users.noreply.github.com>
2016-02-01 23:09:17 -0500

    Update main.js
    Removed element's width and height from the addEventListener

--- views/js/main.js
+++ views/js/main.js
@@ -564,8 +564,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px"; 
+   // Removed element's width and height
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
